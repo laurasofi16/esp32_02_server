4b12546bbf06e7927430ed8f978fe4c8ef380930
alvaro-salazar <ah.salazar@uniandes.edu.co>
2023-03-24 16:20:19 -0500

    Solucion a problemas de encendido del LED y mejora de metodos de conexion

--- src/libwifi.h
+++ src/libwifi.h
@@ -1,1 +1,4 @@
+#include <WiFi.h>
 void conectarWifi(const char * ssid, const char * password);
+void scanNetworks();
+String getTipoCifrado(wifi_auth_mode_t tipoCifrado);
--- src/main.cpp
+++ src/main.cpp
@@ -26,35 +26,42 @@ void setup() {
  * 
  */
 void loop() {
+
   WiFiClient cliente = servidor.available(); //Devolvemos un cliente TCP por wifi conectado a servidor
 
-  
   if(cliente){  //PASO 1: Si existe un cliente conectado?
+    String mensaje="";
     Serial.println("Se conecto un cliente");
     while(cliente.connected()){  //PASO 2: Mientras el cliente este conectado, reciba peticiones (requests)
-       String mensaje;
        if(cliente.available()){ //PASO 3: Verifico si el cliente nos ha enviado un caracter
         char letra = cliente.read();  //PASO 4: Leemos el caracter
         Serial.print(letra);
         if(letra == '\n'){  // Si el caracter es un enter (\n\r: 0x10 y 0x13)
-          //PASO 6: Devolvemos una respuesta (response) HTTP
-          cliente.println("HTTP/1.1 200 OK");
-          cliente.println("Content-type:text/html");
-          cliente.println();  //Enviamos una linea vacia indica al protocolo HTTP que la cabecera finalizo  
-          cliente.println("Haga clic <a href=\"/H\">aqui</a> para encender el LED");
-          cliente.println("Haga clic  <a href=\"/L\">aqui</a> para apagar el LED");
-          cliente.println();  //Finalizamos el envio del cuerpo (body) de la pagina
-          break;
-        } else {
-          if (letra != '\r')
+          if(mensaje.length()==0){            //PASO 6: Devolvemos una respuesta (response) HTTP
+              cliente.println("HTTP/1.1 200 OK");
+              cliente.println("Content-type:text/html");
+              cliente.println();  //Enviamos una linea vacia indica al protocolo HTTP que la cabecera finalizo  
+
+              cliente.println("Haga clic <a href=\"/H\">aqui</a> para encender el LED <br>");
+              cliente.println("Haga clic  <a href=\"/L\">aqui</a> para apagar el LED");
+              cliente.println();  //Finalizamos el envio del cuerpo (body) de la pagina
+              break;
+          } else {
+            mensaje="";
+          }
+        } else if (letra != '\r'){
              mensaje+=letra;  //PASO 5: Concatenamos cada caracter que esta llegando en mensaje
         }
+
+        if(mensaje.endsWith("GET /H")){
+           digitalWrite(2, HIGH);
+        }
+        else if(mensaje.endsWith("GET /L")){   
+          digitalWrite(2, LOW);
+        }
        }
-
     }
-
-
+    Serial.println("Cerrando conexion");
+    cliente.stop();
   }
-
-
 }
